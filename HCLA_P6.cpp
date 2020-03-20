0f9b329807b9f4ddd8a8cb779e594b99a50c3e66
luis <luis.alberto.hernandez.cozatl@uabc.edu.mx>
2020-03-19 21:17:11 -0700

    Creacion de cadena2

--- HCLA_P6.cpp
+++ HCLA_P6.cpp
@@ -123,4 +123,166 @@ void cadena_1(char *frase)
 	printf("Raros%d\n",raros);
 }
 
+//*********** PROGRAMA -
+void programa2(void)
+{
+   system ("cls");
+   printf("********* Cadena2 ***********\n\n");
+    char cadena[]="ENSENADA";
+
+         printf("Salida  1: ");
+          Salida1(cadena);
+         printf("\nSalida  2: ");
+          Salida2(cadena);
+         printf("\nSalida  3: ");
+          Salida3(cadena);
+         printf("\n\nSalida  4: ");
+          Salida4(cadena);
+         printf("\n\nSalida  5: \n");
+          Salida5(cadena);
+        printf("\nSalida  6: \n");
+          Salida6(cadena);
+         printf("\nSalida  7: \n");
+          Salida7(cadena);
+         printf("\nSalida  8: \n");
+          Salida8(cadena);
+         printf("\nSalida  9: \n");
+          Salida9(cadena);
+         printf("\nSalida  10: \n");
+          Salida10(cadena);
+
+  printf("\n\n");
+
+     system("pause");
+}
+
+
+//*********** FUNCIONES:
+void Salida1(char *cadena)
+{
+	printf("%s", cadena);
+
+}
+void Salida2(char *cadena)
+{
+	char cadena2[10];
+	strcpy(cadena2, cadena);
+	printf("%s", strrev(cadena2));
+}
+void Salida3(char *cadena)
+{
+	int i;
+	char cadena3[10];
+	strcpy(cadena3, cadena);
+
+	 for(i=0; i<cadena3[i]; i++)
+	 {
+	 	printf("\n\t%c", cadena3[i]);
+	 }
+}
+void Salida4(char *cadena)
+{
+	int i;
+	char cadena4[10];
+	strcpy(cadena4, cadena);
+	 strrev(cadena4);
+	 for(i=0; i<cadena[i]; i++)
+	 {
+	 	printf("\n\t%c", cadena4[i]);
+	 }
+}
+void Salida5(char *cadena)
+{
+   int i=0;
+   char cadena5[10];
+    strcpy(cadena5, cadena);
+
+    for(i=strlen(cadena5); i>0; i--)
+     {
+     	cadena5[i]=' ';
+     	puts(cadena5);
+	  }
+}
+void Salida6(char *cadena)
+{
+  int i=0;
+   char cadena6[10];
+    strcpy(cadena6, cadena);
+    strrev(cadena6);
+
+     for(i=strlen(cadena6); i>0; i--)
+     {
+     	cadena6[i]=' ';
+     	puts(cadena6);
+	  }
+}
+void Salida7(char *cadena)
+{
+	int i;
+   char cadena7[10];
+    strcpy(cadena7, cadena);
+   // strrev(cadena7);
+
+     for(i=0; i<strlen(cadena7); i++)
+     {
+	    puts(cadena7);
+     	cadena7[i]=' ';
+	  }
+}
+void Salida8(char *cadena)
+{
+	int i;
+   char cadena8[10];
+    strcpy(cadena8, cadena);
+    strrev(cadena8);
+
+     for(i=0; i<strlen(cadena8); i++)
+     {
+	    puts(cadena8);
+     	cadena8[i]=' ';
+	  }
+}
+void Salida9(char *cadena)
+{
+
+   int i;
+   char txt[10];
+
+    strcpy(txt, cadena);
+
+     for(i=0; i<strlen(txt); i++)
+     {
+       if(txt[i]=='B'||txt[i]=='C'||txt[i]=='D'||txt[i]=='F'||
+	          txt[i]=='G'||txt[i]=='H'||txt[i]=='J'||txt[i]=='K'||
+			  txt[i]=='L'||txt[i]=='M'||txt[i]=='N'||txt[i]=='Ñ'||
+			  txt[i]=='P'||txt[i]=='Q'||txt[i]=='R'||txt[i]=='S'||
+			  txt[i]=='T'||txt[i]=='V'||txt[i]=='W'||txt[i]=='X'||
+			  txt[i]=='Y'||txt[i]=='Z'
+		 );
+		  else
+		  {
+		  	txt[i]= ' ';
+		  }
+	  }
+	  puts(txt);
+}
+
+void Salida10(char *cadena)
+{
+		int i;
+   char txt[10];
+
+    strcpy(txt, cadena);
+
+     for(i=0; i<strlen(txt); i++)
+     {
+       if(txt[i]=='A'||txt[i]=='E'||txt[i]=='I'||txt[i]=='O'||txt[i]=='U');
+
+		 else
+		  {
+		  	txt[i]= ' ';
+		  }
+	  }
+	  puts(txt);
+}
 
